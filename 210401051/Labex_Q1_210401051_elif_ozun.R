209b3a189a80e1d56c1eab27de9ed74c46bd3919
Elif Özün <ozunelif0@gmail.com>
2023-11-11 17:27:29 +0300

    1.1, testleri ile tamamlandı

---
+++ 210401051/Labex_Q1_210401051_elif_ozun.R
@@ -0,0 +1,10 @@
+# 1.1)
+
+install.packages('readr')
+library (readr)
+
+getwd()
+setwd("C:/Users/LENOVO/Desktop/gitRepos/laboratuarEgzersizi/Vize/210401051")
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+dosya <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = dosya, method = "auto")
